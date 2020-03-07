b5bce5006a6576899f01a211e2701fc2ce250fed
Philip Ganchev <ermag.bg@gmail.com>
2020-03-07 21:50:54 +0200

    New items and scoring system.

--- src/utils/config.js
+++ src/utils/config.js
@@ -13,22 +13,22 @@ export const CONFIG = Object.freeze({
     item: {
         width: 80, // in pixels
         height: 80, // in pixels
-        pointsPenalty: -30
+        pointsPenalty: -100
     },
-    itemsCount: 14,
+    itemsCount: 15,
     cardsCount: 3, // How many cards to generate
     charsCount: 3, // How many characters to generate
     levels: [
         {
             id: 1,
             board: [
-                [0, 0, 0, 1, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0]
+                [1, 0, 0, 1, 0],
+                [1, 0, 1, 0, 0],
+                [0, 0, 1, 0, 1],
+                [0, 0, 0, 0, 1],
+                [0, 1, 0, 0, 0]
             ],
-            chars: [0, 1, 2],
+            chars: [0],
             cards: [
                 [
                     [1, 1],
@@ -48,38 +48,92 @@ export const CONFIG = Object.freeze({
     chars: [
         { 
             id: 0, 
-            name: "Kifla", 
-            items: [{ id: 0, pt: 100 }, { id: 1, pt: -50 }], 
+            name: "ET", 
+            items: [
+                { id: 0, pt: 150 },
+                { id: 1, pt: 100 },
+                { id: 2, pt: 50 },
+                { id: 9, pt: -150 },
+                { id: 10, pt: -150 },
+                { id: 12, pt: -300 },
+                { id: 14, pt: -300 },
+                { id: 13, pt: 500 }
+            ], 
             talk: { hi: "Hello", win: "That is great", lose: "You suck" } 
         },
         { 
             id: 1, 
-            name: "Kifla", 
-            items: [{ id: 0, pt: 100 }, { id: 1, pt: -50 }], 
+            name: "Robot", 
+            items: [
+                { id: 3, pt: 150 },
+                { id: 4, pt: 100 },
+                { id: 5, pt: 50 },
+                // { id: 9, pt: -150 },
+                // { id: 10, pt: -150 },
+                { id: 12, pt: -300 },
+                { id: 14, pt: -300 },
+                { id: 13, pt: 500 }
+            ], 
             talk: { hi: "Hello", win: "That is great", lose: "You suck" } 
         },
         { 
             id: 2, 
             name: "Kifla", 
-            items: [{ id: 0, pt: 100 }, { id: 1, pt: -50 }], 
+            items: [
+                { id: 6, pt: 150 },
+                { id: 7, pt: 100 },
+                { id: 8, pt: 50 },
+                { id: 11, pt: -150 },
+                { id: 2, pt: -150 },
+                { id: 12, pt: -300 },
+                { id: 14, pt: -300 },
+                { id: 13, pt: 500 }
+            ], 
             talk: { hi: "Hello", win: "That is great", lose: "You suck" } 
         },
         { 
             id: 3, 
-            name: "Kifla", 
-            items: [{ id: 0, pt: 100 }, { id: 1, pt: -50 }], 
+            name: "Alien", 
+            items: [
+                { id: 8, pt: 150 },
+                { id: 2, pt: 100 },
+                { id: 3, pt: 50 },
+                { id: 7, pt: -150 },
+                { id: 0, pt: -150 },
+                { id: 12, pt: -300 },
+                { id: 14, pt: -300 },
+                { id: 13, pt: 500 }
+            ], 
             talk: { hi: "Hello", win: "That is great", lose: "You suck" } 
         },
         { 
             id: 4, 
-            name: "Kifla", 
-            items: [{ id: 0, pt: 100 }, { id: 1, pt: -50 }], 
+            name: "Granny", 
+            items: [
+                { id: 9, pt: 150 },
+                { id: 10, pt: 100 },
+                { id: 4, pt: 50 },
+                // { id: 7, pt: -150 },
+                // { id: 0, pt: -150 },
+                { id: 12, pt: -300 },
+                { id: 14, pt: -300 },
+                { id: 13, pt: 500 }
+            ], 
             talk: { hi: "Hello", win: "That is great", lose: "You suck" } 
         },
         { 
             id: 5, 
-            name: "Kifla", 
-            items: [{ id: 0, pt: 100 }, { id: 1, pt: -50 }], 
+            name: "Santa", 
+            items: [
+                { id: 5, pt: 150 },
+                { id: 11, pt: 100 },
+                { id: 0, pt: 50 },
+                { id: 4, pt: -150 },
+                { id: 3, pt: -150 },
+                { id: 12, pt: -300 },
+                { id: 14, pt: -300 },
+                { id: 13, pt: 500 }
+            ], 
             talk: { hi: "Hello", win: "That is great", lose: "You suck" } 
         }
     ]
