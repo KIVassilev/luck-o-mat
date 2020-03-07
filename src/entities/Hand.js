99f2dc78705ff579f302c23551cca67e40925e47
Konstantin Vassilev <kiv@e2p.io>
2020-03-07 16:09:38 +0200

    Hand grabbing stuff

--- src/entities/Hand.js
+++ src/entities/Hand.js
@@ -1,15 +1,30 @@
 import { CONFIG } from '../utils/config'
 
-class Board extends Phaser.GameObjects.Container {
+class Hand extends Phaser.GameObjects.Container {
     constructor(scene, options) {
         super(scene);
         this.x = options.x;
         this.y = options.y;
         this.hand = scene.add.sprite(0, 0, 'hand');
+        this.hand.setOrigin(0.5,0.12);
+        this.rope = scene.add.sprite(0, 0, 'rope');
+        this.rope.setOrigin(0.5,1);
         this.add(this.hand);
+        this.add(this.rope);
         scene.add.existing(this);
+    }
+    pick(x, y) {
+      this.hand.setFrame(1);
+      this.scene.tweens.add({
+          targets: this,
+          x: x,
+          y: y,
+          duration: 2000,
+          ease: 'Linear',
+          onComplete: function(tween, targets) { tween.targets[0].hand.setFrame(2);}
+        });
     }
 }
 
-export default Board
+export default Hand
 
