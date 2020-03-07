ef2ad17c8050c6e5cdfe32c862147197f37119f8
Konstantin Vassilev <kiv@e2p.io>
2020-03-07 16:25:35 +0200

    Fix picking hand

--- src/entities/Hand.js
+++ src/entities/Hand.js
@@ -6,23 +6,26 @@ class Hand extends Phaser.GameObjects.Container {
         this.x = options.x;
         this.y = options.y;
         this.hand = scene.add.sprite(0, 0, 'hand');
-        this.hand.setOrigin(0.5,0.12);
-        this.rope = scene.add.sprite(0, 0, 'rope');
+        this.rope = scene.add.sprite(0, 15-this.hand.height/2, 'rope');
         this.rope.setOrigin(0.5,1);
         this.add(this.hand);
         this.add(this.rope);
         scene.add.existing(this);
     }
-    pick(x, y) {
-      this.hand.setFrame(1);
-      this.scene.tweens.add({
-          targets: this,
-          x: x,
-          y: y,
-          duration: 2000,
-          ease: 'Linear',
-          onComplete: function(tween, targets) { tween.targets[0].hand.setFrame(2);}
+    pickItem(item) {
+        this.item = item;
+        this.hand.setFrame(1);
+        this.scene.tweens.add({
+            targets: this,
+            x: item.x,
+            y: item.y,
+            duration: 2000,
+            ease: 'Linear',
+            onComplete: this.onPick.bind(this)
         });
+    }
+    onPick(tween) {
+        tween.targets[0].hand.setFrame(2);
     }
 }
 
