993ff1c18a3d20598bbffa2100231fca952346b8
Konstantin Vassilev <kiv@e2p.io>
2020-03-08 12:26:51 +0200

    Items bounce on fall

--- src/entities/Hand.js
+++ src/entities/Hand.js
@@ -64,7 +64,7 @@ class Hand extends Phaser.GameObjects.Container {
                 y: this.dropPos.y,
                 scale: 0.8,
                 duration: 800,
-                ease: 'Expo'
+                ease: 'Bounce'
             });
         }
     }
